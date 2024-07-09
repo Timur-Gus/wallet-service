611a43f0b3bbc3fbc11b1f3a2982037e26c9faa4
Timur-Gus <timurka24478@gmail.ru>
2024-07-09 16:13:15 +0300

    Initial commit

---
+++ src/main/java/com/gusainovt/wallets_service/WalletsServiceApplication.java
@@ -0,0 +1,13 @@
+package com.gusainovt.wallets_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WalletsServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WalletsServiceApplication.class, args);
+	}
+
+}
