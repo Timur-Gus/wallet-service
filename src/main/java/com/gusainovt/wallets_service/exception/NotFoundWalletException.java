107053a727a70c19a3a52d4e2dfc77fcb030f694
Timur-Gus <145484686+Timur-Gus@users.noreply.github.com>
2024-07-11 09:49:30 +0300

    Revert "Complete project"

--- src/main/java/com/gusainovt/wallets_service/exception/NotFoundWalletException.java
+++
@@ -1,8 +0,0 @@
-package com.gusainovt.wallets_service.exception;
-
-import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.ResponseStatus;
-
-@ResponseStatus(HttpStatus.NOT_FOUND)
-public class NotFoundWalletException extends RuntimeException{
-}
